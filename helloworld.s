8cbca8c59d63a7af1690f2f38b2c51d0d75fc144
Duane King <duane.king@duanefking.com>
2020-02-15 03:55:44 -0700

    Final commit :sunglasses:

--- helloworld.s
+++ helloworld.s
@@ -1,1 +1,15 @@
-b68ae2ee-4fe1-11ea-8868-30d16be3d116
+; 32-bit Linux implementation
+section     .text
+global      _start
+_start:
+    mov     edx,len
+    mov     ecx,msg
+    mov     ebx,1 ; stdout
+    mov     eax,4 ; sys_write id
+    int     0x80
+
+    mov     eax,1 ; exit id
+    int     0x80
+section     .data
+msg     db  'Hello, world!',0xa,0x0 ;not sure if null needed, might as well as have it...
+len     equ $ - msg
